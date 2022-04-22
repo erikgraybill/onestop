6f34b51a4ba81ccad02ac28aaefbe074e28a20df
Erik <graybiller@gmail.com>
2022-04-22 13:38:29 -0400

    updates

--- app/src/main/java/hcdd340/finalproject/onestop/MainActivity.java
+++ app/src/main/java/hcdd340/finalproject/onestop/MainActivity.java
@@ -1,12 +1,16 @@
 package hcdd340.finalproject.onestop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "ONESTOP_MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +23,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        int eventSourceId = view.getId();
+        Log.d(TAG, String.format("event source id: %s", eventSourceId));
 
+        if (eventSourceId == R.id.PassWallet) {
+            handlePassWalletClick();
+        }/* else if (eventSourceId == R.id.button_details) {
+            handleDetailsButtonClick();
+        } else if (eventSourceId == R.id.psu_logo) {
+            handleLogoClick();
+        } else if (eventSourceId == R.id.id_label) {
+            handleIDLabelClick();
+        }*/
+        else {
+            Log.d(TAG, String.format("Unknown click event source: %s", eventSourceId));
+        }
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
+    private void handlePassWalletClick() {
+        //Intent intent = new Intent(this, LogInActivity.class);
+        //startActivity(intent);
     }
 }
